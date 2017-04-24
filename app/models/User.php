4abb1d9dec62de2c077e543a26c06d4bc19f3494
pla.olliver@gmail.com <pla.olliver@gmail.com>
2017-04-23 21:53:29 -0400

    Add Cadastro de Usuario

---
+++ app/models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+use \HXPHP\System\Tools;
+
+class User extends \HXPHP\System\Model {
+
+	public static function cadastrar(array $post){
+		
+		$role = Role::find_by_role('user');
+
+		$post = array_merge($post, array(
+			'role_id' => $role->id,
+			'status' =>1
+		));
+
+		$password = Tools::hashHX($post['password']);
+
+		$post = array_merge($post, $password);
+
+		return self::create($post);
+	}
+}
