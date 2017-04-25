272cb05dd9e1f922c3fa5147687a33898b797ec6
pla.olliver@gmail.com <pla.olliver@gmail.com>
2017-04-25 14:59:51 -0400

    Add action sair

--- app/controllers/RecuperarController.php
+++ app/controllers/RecuperarController.php
@@ -5,5 +5,14 @@
 */
 class RecuperarController extends \HXPHP\System\Controller
 {
-	
+	public function __construct($configs){
+		parent::__construct($configs);
+
+		$this->load(
+			'Services\Auth',
+			$configs->auth->after_login,
+			$configs->auth->after_logout,
+			true
+		);
+	}
 }
